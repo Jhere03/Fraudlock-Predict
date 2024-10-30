3315dadc6ab7f44c3ce20947d252a04dbdb2a286
Jhere <rodriguezrerick101@gmail.com>
2024-10-30 05:46:33 -0500

    Apis asignadas

--- app.py
+++ app.py
@@ -1,14 +1,23 @@
-
 import os
+
+# Forzar el uso de CPU en lugar de GPU en TensorFlow
+os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
+
+# Importar otras bibliotecas
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import tensorflow as tf
 import numpy as np
-import requests
-from urllib.parse import urlparse
-from datetime import datetime
+from ssl_check import check_ssl
+from url_similarity import check_url_similarity
+from domain_security import check_domain_security
+from populary_domain import check_domain_popularity
+from metadata_check import check_metadata
 from conect_bd import get_db_connection
 from report_manager import ReportManager
+from urllib.parse import urlparse
+from datetime import datetime
+import requests
 
 app = Flask(__name__)
 CORS(app)
@@ -140,6 +149,27 @@ def predict():
     connection.close()
 
     return jsonify({"probability": float(result), "features": features_list}), 200
+
+# Registrar las rutas de todas las APIs
+@app.route('/api/check_ssl', methods=['POST'])
+def check_ssl_route():
+    return check_ssl()
+
+@app.route('/api/check_url_similarity', methods=['POST'])
+def check_url_similarity_route():
+    return check_url_similarity()
+
+@app.route('/api/check_domain_security', methods=['POST'])
+def check_domain_security_route():
+    return check_domain_security()
+
+@app.route('/api/check_domain_popularity', methods=['POST'])
+def check_domain_popularity_route():
+    return check_domain_popularity()
+
+@app.route('/api/check_metadata', methods=['POST'])
+def check_metadata_route():
+    return check_metadata()
 
 # Ejecutar la aplicación Flask
 if __name__ == "__main__":
